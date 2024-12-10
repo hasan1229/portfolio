24dc1779d48cdea32b2246d3234039c55f878cb8
tasin hasan <rabialhasan.1229@gmail.com>
2024-12-10 21:47:07 +0600

    navbar of portfolio

---
+++ src/component/Home.jsx
@@ -0,0 +1,17 @@
+
+
+function Home() {
+  return (
+<>
+<div className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20">
+      <div>rfth</div>
+
+
+      <div>ftgh</div>
+    </div>
+
+</>
+  )
+}
+
+export default Home
