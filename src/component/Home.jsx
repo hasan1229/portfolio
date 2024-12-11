75a07d64d714dfc6bccf72b972c9e1c00e0edac5
tasin hasan <rabialhasan.1229@gmail.com>
2024-12-11 10:18:27 +0600

    home section partiall

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,16 +1,60 @@
-
+import React from "react";
+import Typical from "react-typical";
+import pic from '../../public/photo.avif'
+import { FaFacebook } from "react-icons/fa6";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
+import { GrReactjs } from "react-icons/gr";
+import { MdOutlinePhp } from "react-icons/md";
+import { RiTailwindCssFill } from "react-icons/ri";
 
 function Home() {
   return (
-<>
-<div className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20">
-      <div>rfth</div>
+    <>
+      <div className="max-w-screen-2xl container mx-auto px-20 md:px-20 my-20 flex justify-between items-center  py-36" >
+        <div className="w-1/2">
+          <div ><p>Welcome in my feed</p>
+            <h2 className="text-3xl">
+              Hello, I am a{" "}
+              <span style={{ color: 'red' }}>
+                <Typical
+                  loop={Infinity}
+                  wrapper="span"
+                  steps={["programmer", 1000, "developer", 1000, "designer", 1000]}
+                /></span>
+            </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor <br></br>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            </p></div>
+
+          <div className="flex justify-between  ">
+            <div className="flex-row ">
+              <div className="py-4">available on</div>
+            <div className="flex justify-between py-2">
+              <FaFacebook size={24}/>
+              <IoLogoWhatsapp size={24}/>
+              <FaInstagram size={24}/>
+              
+            </div>
+            </div>
+
+            <div className="flex-row">
+              <div className="py-4">available on</div>
+            <div className="flex justify-between py-2">
+              <GrReactjs size={24}/>
+              <MdOutlinePhp size={24}/>
+              <RiTailwindCssFill size={24}/>
+              
+            </div>
+            </div>
+          </div>
+
+        </div>
 
 
-      <div>ftgh</div>
-    </div>
+        <div><img className='h-96 w-96 rounded-full mr-20' src={pic} alt="" /></div>
+      </div>
 
-</>
+    </>
   )
 }
 
